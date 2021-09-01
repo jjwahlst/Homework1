dae1f7bfa3ad5403547a6c02449dbfaf6a48f5fe
jjwahlst <jjwahlst@asu.edu>
2021-08-31 21:53:25 -0700

    smh. I just didn't put a run path... I'm very new to this. This is a lot of new basic stuff for me. I got a different answer than you did in class, however, I believe you did x0-x2 instead of x0-x1. I'll change mine to see if i get the same answer, then change it back to what i think is the correct notation.

--- homework1.py
+++ homework1.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy.optimize import minimize
 
-fun = lambda x: (x[0] - x[1])**2 + (x[1]+x[2] - 2)**2 + (x[3]-1)**2 + (x[4]-1)**2
+fun = lambda x: (x[0] - x[2])**2 + (x[1]+x[2] - 2)**2 + (x[3]-1)**2 + (x[4]-1)**2
 cons = ({'type': 'eq', 'fun': lambda x: x[0] + 3*x[1]},
         {'type': 'eq', 'fun': lambda x: x[2] + x[3] - 2*x[4]},
         {'type': 'eq', 'fun': lambda x: x[1] - x[4]})
