39b0426d4d7392093b29ae57b45e9f0eea71887b
jjwahlst <jjwahlst@asu.edu>
2021-08-31 22:00:10 -0700

    This is what I'm going with. I think I have everything entered correctly. My answer is x: array([-0.76725781,  0.2557526 ,  0.62762513, -0.11611992,  0.2557526 ]). Thank you.

--- homework1.py
+++ homework1.py
@@ -1,11 +1,12 @@
 import numpy as np
 from scipy.optimize import minimize
 
-fun = lambda x: (x[0] - x[2])**2 + (x[1]+x[2] - 2)**2 + (x[3]-1)**2 + (x[4]-1)**2
+fun = lambda x: (x[0] - x[1])**2 + (x[1]+x[2] - 2)**2 + (x[3]-1)**2 + (x[4]-1)**2
 cons = ({'type': 'eq', 'fun': lambda x: x[0] + 3*x[1]},
         {'type': 'eq', 'fun': lambda x: x[2] + x[3] - 2*x[4]},
         {'type': 'eq', 'fun': lambda x: x[1] - x[4]})
 bnds = ((-10, 10), (-10, 10), (-10, 10), (-10, 10), (-10, 10))
 x0 = np.array([1, 3, 4, 5, 6])
 res = minimize(fun, x0, method='SLSQP', bounds=bnds, constraints=cons)
-print(res.x)
+
+print(res)
