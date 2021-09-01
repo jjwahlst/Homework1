a3e36d9d2456cfa174e91e9329de7718e1802f13
jjwahlst <jjwahlst@asu.edu>
2021-08-31 21:47:46 -0700

    This is closer. It runs in the console but not as a script...?

--- homework1.py
+++ homework1.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 from scipy.optimize import minimize
 
 fun = lambda x: (x[0] - x[1])**2 + (x[1]+x[2] - 2)**2 + (x[3]-1)**2 + (x[4]-1)**2
@@ -6,4 +6,5 @@ cons = ({'type': 'eq', 'fun': lambda x: x[0] + 3*x[1]},
         {'type': 'eq', 'fun': lambda x: x[2] + x[3] - 2*x[4]},
         {'type': 'eq', 'fun': lambda x: x[1] - x[4]})
 bnds = ((-10, 10), (-10, 10), (-10, 10), (-10, 10), (-10, 10))
-res = minimize(fun, (1, 2, 3, 4, 5, 6), method='SLSQP', bounds=bnds, constraints=cons)
+x0 = np.array([1, 3, 4, 5, 6])
+res = minimize(fun, x0, method='SLSQP', bounds=bnds, constraints=cons)
